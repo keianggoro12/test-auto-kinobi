360cbe139cdd92e45265c47ac1bee855db959cd8
shasha2929 <aisharach.eat@gmail.com>
2025-02-01 19:02:22 +0700

    register student

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -6,73 +6,8 @@ import { stepOnBoarding } from "../pages/stepOnboarding";
 const dataURL = require("../testData/dataURL.json");
 const dataLogin = require("../testData/dataLogin.json");
 
-test.describe("TS-1: Login", () => {
-  let page, context, loginPage, touPanel, onBoardPanel;
-
-  // Membuat context dan page sebelum semua tes
-  test.beforeAll(async ({ browser }) => {
-    context = await browser.newContext();
-    page = await context.newPage();
-    loginPage = new stepLogin(page);
-    touPanel = new stepTou(page);
-    onBoardPanel = new stepOnBoarding(page);
-
-    console.log("Opening URL...");
-    await loginPage.openUrl(dataURL.URL);
-    console.log("URL opened successfully.");
-  });
-
-  // TC-1: Test untuk login dengan disable login - no data
-  test("TC-1: Login with disabled button", async () => {
-    await loginPage.click_buttonLoginStudent();
-    await loginPage.disabled_buttonLogin();
-  });
-
-  // // TC-2: Test untuk login dengan password tidak valid
-  test("TC-2: Login with invalid password", async () => {
-    await loginPage.fill_inputEmail(dataLogin.email);
-    await loginPage.fill_inputinvalidPassword(dataLogin.invalid_password);
-    await loginPage.click_buttonSignIn();
-    await loginPage.visible_toastinvalidPassword();
-  });
-
-  // TC-3: Test untuk login dengan kredensial valid
-  test("TC-3: Login with valid credentials as student", async () => {
-    await loginPage.fill_inputEmail(dataLogin.email);
-    await loginPage.fill_inputPassword(dataLogin.password);
-    await loginPage.click_buttonSignIn();
-    await loginPage.visible_toastSuccessLogin();
-    await touPanel.handle_termsOfUse();
-    await onBoardPanel.handle_OnbBoarding();
-  });
-
-  // TC-4: Test untuk logout
-  // test("TC-4: Test log out as student", async () => {
-  //   await loginPage.click_threeDotsButton();
-  //   await loginPage.click_logout();
-  //   await loginPage.visible_buttonLogin();
-  // });
-
-  // Menutup semua resource setelah semua tes selesai
-  test.afterAll(async () => {
-    try {
-      console.log("Closing all resources...");
-      if (page) {
-        await page.close();
-        console.log("Page closed.");
-      }
-      if (context) {
-        await context.close();
-        console.log("Context closed.");
-      }
-    } catch (error) {
-      console.error("Error during cleanup:", error.message);
-    }
-  });
-});
-
-// test.describe("TS-2: Register", () => {
-//   let page, context, loginPage, touPanel, registerPage;
+// test.describe("TS-1: Login", () => {
+//   let page, context, loginPage, touPanel, onBoardPanel;
 
 //   // Membuat context dan page sebelum semua tes
 //   test.beforeAll(async ({ browser }) => {
@@ -80,7 +15,7 @@ test.describe("TS-1: Login", () => {
 //     page = await context.newPage();
 //     loginPage = new stepLogin(page);
 //     touPanel = new stepTou(page);
-//     registerPage = new stepRegister(page);
+//     onBoardPanel = new stepOnBoarding(page);
 
 //     console.log("Opening URL...");
 //     await loginPage.openUrl(dataURL.URL);
@@ -88,10 +23,35 @@ test.describe("TS-1: Login", () => {
 //   });
 
 //   // TC-1: Test untuk login dengan disable login - no data
-//   test("TC-1: Register", async () => {
-//     await registerPage.click_buttonSignUp();
-//     await registerPage.registerAndVerifyEmail();
+//   test("TC-1: Login with disabled button", async () => {
+//     await loginPage.click_buttonLoginStudent();
+//     await loginPage.disabled_buttonLogin();
 //   });
+
+//   // // TC-2: Test untuk login dengan password tidak valid
+//   test("TC-2: Login with invalid password", async () => {
+//     await loginPage.fill_inputEmail(dataLogin.email);
+//     await loginPage.fill_inputinvalidPassword(dataLogin.invalid_password);
+//     await loginPage.click_buttonSignIn();
+//     await loginPage.visible_toastinvalidPassword();
+//   });
+
+//   // TC-3: Test untuk login dengan kredensial valid
+//   test("TC-3: Login with valid credentials as student", async () => {
+//     await loginPage.fill_inputEmail(dataLogin.email);
+//     await loginPage.fill_inputPassword(dataLogin.password);
+//     await loginPage.click_buttonSignIn();
+//     await loginPage.visible_toastSuccessLogin();
+//     await touPanel.handle_termsOfUse();
+//     await onBoardPanel.handle_OnbBoarding();
+//   });
+
+//   // TC-4: Test untuk logout
+//   // test("TC-4: Test log out as student", async () => {
+//   //   await loginPage.click_threeDotsButton();
+//   //   await loginPage.click_logout();
+//   //   await loginPage.visible_buttonLogin();
+//   // });
 
 //   // Menutup semua resource setelah semua tes selesai
 //   test.afterAll(async () => {
@@ -110,3 +70,49 @@ test.describe("TS-1: Login", () => {
 //     }
 //   });
 // });
+
+test.describe("TS-2: Register", () => {
+  let page, context, loginPage, touPanel, registerPage;
+
+  // Membuat context dan page sebelum semua tes
+  test.beforeAll(async ({ browser }) => {
+    context = await browser.newContext();
+    page = await context.newPage();
+    loginPage = new stepLogin(page);
+    touPanel = new stepTou(page);
+    registerPage = new stepRegister(page);
+
+    console.log("Opening URL...");
+    await loginPage.openUrl(dataURL.URL);
+    console.log("URL opened successfully.");
+  });
+
+  // TC-1: Test untuk login dengan disable login - no data
+  test("TS-: Register", async () => {
+    await registerPage.click_buttonSignUp();
+    await registerPage.verifySignupModal();
+    await registerPage.fill_inputFirstName(dataLogin.firstname);
+    await registerPage.fill_inputLastName(dataLogin.lastname);
+    await registerPage.registerAndVerifyEmail();
+    await registerPage.fill_inputPassword(dataLogin.password);
+    await registerPage.click_signupModal();
+    
+  });
+
+  // Menutup semua resource setelah semua tes selesai
+  test.afterAll(async () => {
+    try {
+      console.log("Closing all resources...");
+      if (page) {
+        await page.close();
+        console.log("Page closed.");
+      }
+      if (context) {
+        await context.close();
+        console.log("Context closed.");
+      }
+    } catch (error) {
+      console.error("Error during cleanup:", error.message);
+    }
+  });
+});
